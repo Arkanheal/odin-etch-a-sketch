f28cff7ec9ecb732745c73d1870c019ce3ff4867
Arkanheal <27277903+Arkanheal@users.noreply.github.com>
2023-06-23 22:20:41 +0200

    Add random coloring and darkening
    Now that colors are permanent, I implemented a random color choice
    that darkens by 10% each time you mouse over it. When it becomes
    eventually full black (brightness(0%)), it choose another random
    color and reset brightness.

--- js/script.js
+++ js/script.js
@@ -2,21 +2,36 @@
 const containerDiv = document.querySelector(".container");
 
 // Utility functions
-function updateColor(event, color) {
-  event.target.style.backgroundColor = color;
+function updateColor(event) {
+  const currentBackgroundColor = event.target.style.backgroundColor;
+
+  const filter = event.target.style.filter;
+  const brightnessRe = /brightness\((\d+)%\)/;
+  const brightness = filter.match(brightnessRe);
+
+  if (currentBackgroundColor && !filter) {
+    event.target.style.filter = "brightness(90%)";
+  } else if (filter && parseInt(brightness[1]) > 0 && currentBackgroundColor) {
+    event.target.style.filter = `brightness(${parseInt(brightness[1]) - 10}%)`;
+  } else {
+    const newColor = Math.floor(Math.random() * 16777215).toString(16);
+    event.target.style.backgroundColor = `#${newColor}`;
+    event.target.style.filter = "brightness(100%)";
+  }
+
 }
 
 function generateGrid(size) {
   while (containerDiv.firstChild) {
     containerDiv.removeChild(containerDiv.lastChild);
   }
+
   const flexBasis = 100 / size;
   for (let i = 0; i < size * size; ++i) {
     const createdDiv = document.createElement("div");
     createdDiv.className = "drawing-square";
     createdDiv.style.flex = `1 1 ${flexBasis}%`;
-    createdDiv.addEventListener("mouseover", (e) => updateColor(e, "red"));
-    createdDiv.addEventListener("mouseleave", (e) => updateColor(e, "white"));
+    createdDiv.addEventListener("mouseover", (e) => updateColor(e));
     containerDiv.appendChild(createdDiv);
   }
 }
@@ -29,7 +44,6 @@ sizeButton.addEventListener("click", () => {
   do {
     sizePrompt = prompt("Enter a size");
     size = parseInt(sizePrompt);
-    console.log(size);
   } while (sizePrompt !== null && (isNaN(size) || size < 4))
   if (sizePrompt !== null) {
     generateGrid(size);
