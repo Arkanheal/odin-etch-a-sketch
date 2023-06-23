d574183eff92094eb34467f1be9fddfb1994fb96
Arkanheal <27277903+Arkanheal@users.noreply.github.com>
2023-06-23 19:20:33 +0200

    Add pretty mouseover

--- js/script.js
+++ js/script.js
@@ -5,3 +5,14 @@ for (let i = 0; i < 16; ++i) {
   createdDiv.className = "drawing-square";
   containerDiv.appendChild(createdDiv);
 }
+
+const createdDivs = document.querySelectorAll(".drawing-square");
+
+for (div of createdDivs) {
+  div.addEventListener("mouseover", (e) => updateColor(e, "red"));
+  div.addEventListener("mouseleave", (e) => updateColor(e, "white"));
+}
+
+function updateColor(event, color) {
+  event.target.style.backgroundColor = color;
+}
