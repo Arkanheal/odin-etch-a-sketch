e7dd5801315c6d83df9d31435becf4e0b92b22d3
Arkanheal <27277903+Arkanheal@users.noreply.github.com>
2023-06-23 20:55:14 +0200

    Add resize button

--- js/script.js
+++ js/script.js
@@ -1,18 +1,37 @@
+// Creates the square
 const containerDiv = document.querySelector(".container");
 
-for (let i = 0; i < 16; ++i) {
-  const createdDiv = document.createElement("div");
-  createdDiv.className = "drawing-square";
-  containerDiv.appendChild(createdDiv);
-}
-
-const createdDivs = document.querySelectorAll(".drawing-square");
-
-for (div of createdDivs) {
-  div.addEventListener("mouseover", (e) => updateColor(e, "red"));
-  div.addEventListener("mouseleave", (e) => updateColor(e, "white"));
-}
-
+// Utility functions
 function updateColor(event, color) {
   event.target.style.backgroundColor = color;
 }
+
+function generateGrid(size) {
+  while (containerDiv.firstChild) {
+    containerDiv.removeChild(containerDiv.lastChild);
+  }
+  const flexBasis = 100/size;
+  for (let i = 0; i < size * size; ++i) {
+    const createdDiv = document.createElement("div");
+    createdDiv.className = "drawing-square";
+    createdDiv.style.flex = `1 1 ${flexBasis}%`;
+    createdDiv.addEventListener("mouseover", (e) => updateColor(e, "red"));
+    createdDiv.addEventListener("mouseleave", (e) => updateColor(e, "white"));
+    containerDiv.appendChild(createdDiv);
+  }
+}
+
+// Change grid size
+const sizeButton = document.querySelector(".size-button");
+sizeButton.addEventListener("click", () => {
+  let size = -1;
+  let sizePrompt = null;
+  do {
+    sizePrompt = prompt("Enter a size");
+    size = parseInt(sizePrompt);
+    console.log(size);
+    } while (sizePrompt !== null && (isNaN(size) || size < 4))
+  generateGrid(size);
+});
+
+generateGrid(16);
